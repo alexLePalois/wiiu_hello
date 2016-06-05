36941a9a7a75109369ad66d90747f425bbb49c43
alexandre <alexandre@alexandre-1005PX>
2016-06-05 20:57:17 +0200

    Adds hello word screen print

--- src/main.c
+++ src/main.c
@@ -1,6 +1,66 @@
 #include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
 #include "dynamic_libs/os_functions.h"
 #include "dynamic_libs/vpad_functions.h"
+
+#define MAX_CONSOLE_LINES_TV    27
+#define MAX_CONSOLE_LINES_DRC   18
+
+static char * consoleArrayTv[MAX_CONSOLE_LINES_TV];
+static char * consoleArrayDrc[MAX_CONSOLE_LINES_DRC];
+
+void console_printf(const char *format, ...)
+{
+	char * tmp = NULL;
+
+	va_list va;
+	va_start(va, format);
+	if((vasprintf(&tmp, format, va) >= 0) && tmp)
+	{
+	    if(consoleArrayTv[0])
+            free(consoleArrayTv[0]);
+	    if(consoleArrayDrc[0])
+            free(consoleArrayDrc[0]);
+
+        for(int i = 1; i < MAX_CONSOLE_LINES_TV; i++)
+            consoleArrayTv[i-1] = consoleArrayTv[i];
+
+        for(int i = 1; i < MAX_CONSOLE_LINES_DRC; i++)
+            consoleArrayDrc[i-1] = consoleArrayDrc[i];
+
+        if(strlen(tmp) > 79)
+            tmp[79] = 0;
+
+        consoleArrayTv[MAX_CONSOLE_LINES_TV-1] = (char*)malloc(strlen(tmp) + 1);
+        if(consoleArrayTv[MAX_CONSOLE_LINES_TV-1])
+            strcpy(consoleArrayTv[MAX_CONSOLE_LINES_TV-1], tmp);
+
+        consoleArrayDrc[MAX_CONSOLE_LINES_DRC-1] = (tmp);
+	}
+	va_end(va);
+
+    // Clear screens
+    OSScreenClearBufferEx(0, 0);
+    OSScreenClearBufferEx(1, 0);
+
+
+	for(int i = 0; i < MAX_CONSOLE_LINES_TV; i++)
+    {
+        if(consoleArrayTv[i])
+            OSScreenPutFontEx(0, 0, i, consoleArrayTv[i]);
+    }
+
+	for(int i = 0; i < MAX_CONSOLE_LINES_DRC; i++)
+    {
+        if(consoleArrayDrc[i])
+            OSScreenPutFontEx(1, 0, i, consoleArrayDrc[i]);
+    }
+
+	OSScreenFlipBuffersEx(0);
+	OSScreenFlipBuffersEx(1);
+}
 
 /* Entry point */
 int Menu_Main(void)
@@ -12,7 +72,35 @@ int Menu_Main(void)
     InitOSFunctionPointers();
     InitVPadFunctionPointers();
 
+	for(int i = 0; i < MAX_CONSOLE_LINES_TV; i++)
+        consoleArrayTv[i] = NULL;
+
+	for(int i = 0; i < MAX_CONSOLE_LINES_DRC; i++)
+        consoleArrayDrc[i] = NULL;
+
     VPADInit();
+
+    // Prepare screen
+    int screen_buf0_size = 0;
+
+    // Init screen and screen buffers
+    OSScreenInit();
+    screen_buf0_size = OSScreenGetBufferSizeEx(0);
+    OSScreenSetBufferEx(0, (void *)0xF4000000);
+    OSScreenSetBufferEx(1, (void *)(0xF4000000 + screen_buf0_size));
+
+    OSScreenEnableEx(0, 1);
+    OSScreenEnableEx(1, 1);
+
+    // Clear screens
+    OSScreenClearBufferEx(0, 0);
+    OSScreenClearBufferEx(1, 0);
+
+    // Flip buffers
+    OSScreenFlipBuffersEx(0);
+    OSScreenFlipBuffersEx(1);
+
+    console_printf("Hello Word! (Home Button to quit)");
 
     int vpadError = -1;
     VPADData vpad;
@@ -32,6 +120,19 @@ int Menu_Main(void)
         }
 
 		usleep(1000);
+    }
+
+    //! free memory
+	for(int i = 0; i < MAX_CONSOLE_LINES_TV; i++)
+    {
+        if(consoleArrayTv[i])
+            free(consoleArrayTv[i]);
+    }
+
+	for(int i = 0; i < MAX_CONSOLE_LINES_DRC; i++)
+    {
+        if(consoleArrayDrc[i])
+            free(consoleArrayDrc[i]);
     }
 
     return EXIT_SUCCESS;
