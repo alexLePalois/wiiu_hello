275d3c70171ee53b050de5cfc35865d3e8b95b4a
alexandre <alexandre@alexandre-1005PX>
2016-06-08 21:44:37 +0200

    Cleans printing hello word function

--- src/main.c
+++ src/main.c
@@ -5,11 +5,8 @@
 #include "dynamic_libs/os_functions.h"
 #include "dynamic_libs/vpad_functions.h"
 
-#define MAX_CONSOLE_LINES_TV    27
-#define MAX_CONSOLE_LINES_DRC   18
-
-static char * consoleArrayTv[MAX_CONSOLE_LINES_TV];
-static char * consoleArrayDrc[MAX_CONSOLE_LINES_DRC];
+static char * consoleArrayTv;
+static char * consoleArrayDrc;
 
 void console_printf(const char *format, ...)
 {
@@ -19,44 +16,25 @@ void console_printf(const char *format, ...)
 	va_start(va, format);
 	if((vasprintf(&tmp, format, va) >= 0) && tmp)
 	{
-	    if(consoleArrayTv[0])
-            free(consoleArrayTv[0]);
-	    if(consoleArrayDrc[0])
-            free(consoleArrayDrc[0]);
+		if(strlen(tmp) > 79)
+			tmp[79] = 0;
 
-        for(int i = 1; i < MAX_CONSOLE_LINES_TV; i++)
-            consoleArrayTv[i-1] = consoleArrayTv[i];
+		consoleArrayTv = (char*)malloc(strlen(tmp) + 1);
+		if(consoleArrayTv)
+			strcpy(consoleArrayTv, tmp);
 
-        for(int i = 1; i < MAX_CONSOLE_LINES_DRC; i++)
-            consoleArrayDrc[i-1] = consoleArrayDrc[i];
-
-        if(strlen(tmp) > 79)
-            tmp[79] = 0;
-
-        consoleArrayTv[MAX_CONSOLE_LINES_TV-1] = (char*)malloc(strlen(tmp) + 1);
-        if(consoleArrayTv[MAX_CONSOLE_LINES_TV-1])
-            strcpy(consoleArrayTv[MAX_CONSOLE_LINES_TV-1], tmp);
-
-        consoleArrayDrc[MAX_CONSOLE_LINES_DRC-1] = (tmp);
+		consoleArrayDrc = (tmp);
 	}
 	va_end(va);
 
-    // Clear screens
-    OSScreenClearBufferEx(0, 0);
-    OSScreenClearBufferEx(1, 0);
+	// Clear screens
+	OSScreenClearBufferEx(0, 0);
+	OSScreenClearBufferEx(1, 0);
 
-
-	for(int i = 0; i < MAX_CONSOLE_LINES_TV; i++)
-    {
-        if(consoleArrayTv[i])
-            OSScreenPutFontEx(0, 0, i, consoleArrayTv[i]);
-    }
-
-	for(int i = 0; i < MAX_CONSOLE_LINES_DRC; i++)
-    {
-        if(consoleArrayDrc[i])
-            OSScreenPutFontEx(1, 0, i, consoleArrayDrc[i]);
-    }
+	if(consoleArrayTv)
+		OSScreenPutFontEx(0, 0, 0, consoleArrayTv);
+	if(consoleArrayDrc)
+		OSScreenPutFontEx(1, 0, 0, consoleArrayDrc);
 
 	OSScreenFlipBuffersEx(0);
 	OSScreenFlipBuffersEx(1);
@@ -65,76 +43,64 @@ void console_printf(const char *format, ...)
 /* Entry point */
 int Menu_Main(void)
 {
-    //!*******************************************************************
-    //!                   Initialize function pointers                   *
-    //!*******************************************************************
-    //! do OS (for acquire) and sockets first so we got logging
-    InitOSFunctionPointers();
-    InitVPadFunctionPointers();
+	//!*******************************************************************
+	//!                   Initialize function pointers                   *
+	//!*******************************************************************
+	//! do OS (for acquire) and sockets first so we got logging
+	InitOSFunctionPointers();
+	InitVPadFunctionPointers();
 
-	for(int i = 0; i < MAX_CONSOLE_LINES_TV; i++)
-        consoleArrayTv[i] = NULL;
+	consoleArrayTv = NULL;
+	consoleArrayDrc = NULL;
 
-	for(int i = 0; i < MAX_CONSOLE_LINES_DRC; i++)
-        consoleArrayDrc[i] = NULL;
+	VPADInit();
 
-    VPADInit();
+	// Prepare screen
+	int screen_buf0_size = 0;
 
-    // Prepare screen
-    int screen_buf0_size = 0;
+	// Init screen and screen buffers
+	OSScreenInit();
+	screen_buf0_size = OSScreenGetBufferSizeEx(0);
+	OSScreenSetBufferEx(0, (void *)0xF4000000);
+	OSScreenSetBufferEx(1, (void *)(0xF4000000 + screen_buf0_size));
 
-    // Init screen and screen buffers
-    OSScreenInit();
-    screen_buf0_size = OSScreenGetBufferSizeEx(0);
-    OSScreenSetBufferEx(0, (void *)0xF4000000);
-    OSScreenSetBufferEx(1, (void *)(0xF4000000 + screen_buf0_size));
+	OSScreenEnableEx(0, 1);
+	OSScreenEnableEx(1, 1);
 
-    OSScreenEnableEx(0, 1);
-    OSScreenEnableEx(1, 1);
+	// Clear screens
+	OSScreenClearBufferEx(0, 0);
+	OSScreenClearBufferEx(1, 0);
 
-    // Clear screens
-    OSScreenClearBufferEx(0, 0);
-    OSScreenClearBufferEx(1, 0);
+	// Flip buffers
+	OSScreenFlipBuffersEx(0);
+	OSScreenFlipBuffersEx(1);
 
-    // Flip buffers
-    OSScreenFlipBuffersEx(0);
-    OSScreenFlipBuffersEx(1);
+	console_printf("Hello Word! (Home Button to quit)");
 
-    console_printf("Hello Word! (Home Button to quit)");
+	int vpadError = -1;
+	VPADData vpad;
+	int vpadReadCounter = 0;
 
-    int vpadError = -1;
-    VPADData vpad;
-    int vpadReadCounter = 0;
+	while(1)
+	{
+		//! update only at 50 Hz, thats more than enough
+		if(++vpadReadCounter >= 20)
+		{
+			vpadReadCounter = 0;
 
-    while(1)
-    {
-        //! update only at 50 Hz, thats more than enough
-        if(++vpadReadCounter >= 20)
-        {
-            vpadReadCounter = 0;
+			VPADRead(0, &vpad, 1, &vpadError);
 
-            VPADRead(0, &vpad, 1, &vpadError);
-
-            if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & VPAD_BUTTON_HOME))
-                break;
-        }
+			if(vpadError == 0 && ((vpad.btns_d | vpad.btns_h) & VPAD_BUTTON_HOME))
+				break;
+		}
 
 		usleep(1000);
-    }
+	}
 
-    //! free memory
-	for(int i = 0; i < MAX_CONSOLE_LINES_TV; i++)
-    {
-        if(consoleArrayTv[i])
-            free(consoleArrayTv[i]);
-    }
+	//! free memory
+	if(consoleArrayTv) free(consoleArrayTv);
+	if(consoleArrayDrc) free(consoleArrayDrc);
 
-	for(int i = 0; i < MAX_CONSOLE_LINES_DRC; i++)
-    {
-        if(consoleArrayDrc[i])
-            free(consoleArrayDrc[i]);
-    }
-
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
